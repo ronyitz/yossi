69bb98495bf6fc1f61cfbf94e4bf364ec9470ca6
ronyitz <ronchu90@gmail.com>
2020-10-14 13:31:51 +0300

    Updates

---
+++ precache-manifest.a6b5cdfcf69255f5436c38d595fe140d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1983c9e880936dd1ed5f6d8b585948f",
+    "url": "/yossi/index.html"
+  },
+  {
+    "revision": "3305f85d4917f64fe630",
+    "url": "/yossi/static/css/main.aeb1c58e.chunk.css"
+  },
+  {
+    "revision": "5a90fa498e7e55482075",
+    "url": "/yossi/static/js/2.3884fa41.chunk.js"
+  },
+  {
+    "revision": "1a6265a09757315bdbe2199a5aec909b",
+    "url": "/yossi/static/js/2.3884fa41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3305f85d4917f64fe630",
+    "url": "/yossi/static/js/main.5283769b.chunk.js"
+  },
+  {
+    "revision": "23c1d567950d89cf71a8",
+    "url": "/yossi/static/js/runtime-main.d4cc7dea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yossi/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "97b3a7325cec6d23acc3706df3687cfc",
+    "url": "/yossi/static/media/logo1.97b3a732.png"
+  }
+]);
